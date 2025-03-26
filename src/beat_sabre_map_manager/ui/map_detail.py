2ab3e4cbae04924f0863b1f4d267bbba92c6186e
Kamil Democko <kamil.democko@gmail.com>
2025-03-26 23:34:55 +0100

    audio in app not working, added open external

--- src/beat_sabre_map_manager/ui/map_detail.py
+++ src/beat_sabre_map_manager/ui/map_detail.py
@@ -1,5 +1,12 @@
+import atexit
+import os
+import random
+from tempfile import TemporaryDirectory, tempdir
 import base64
 from pathlib import Path
+import shutil
+from typing import Callable
+import uuid
 
 import flet as ft
 
@@ -12,6 +19,8 @@ class MapDetailUI:
             content=ft.Column([]),
             padding=16,
         )
+        self.tempdir = TemporaryDirectory()
+        atexit.register(self.tempdir.cleanup)
 
         self._build_default_content()
     
@@ -24,6 +33,15 @@ class MapDetailUI:
         else:
             return "iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mP8/wcAAwAB/ep2G+IAAAAASUVORK5CYII="
     
+    def _open_audio_file(self, path: str) -> None:
+        audio_path = Path(path)
+
+        if audio_path.exists():
+            temp_audio_path = f"{self.tempdir.name}/{uuid.uuid4()}.ogg"
+            shutil.copy(audio_path, temp_audio_path)
+            os.startfile(temp_audio_path)
+
+
     def build_content(self, detail: MapDetail) -> None:
         col = ft.Column([
             ft.Row([
@@ -39,12 +57,14 @@ class MapDetailUI:
                 ft.Column([
                     ft.TextField(label="Version", read_only=True, value=detail.version),
                     ft.TextField(label="BPM", read_only=True, value=f"{detail.beats_per_minute:.1f}"),
-                    ft.Audio(src=f"file://{detail.song_filename}")
+                    ft.OutlinedButton(text="Open audio file", on_click=lambda _: self._open_audio_file(detail.song_filename))
                 ]),
             ]),
             ft.TextField(label="Name", read_only=True, value=detail.song_name),
-            ft.TextField(label="Song author", read_only=True, value=detail.song_author_name),
-            ft.TextField(label="Map author", read_only=True, value=detail.level_author_name),
+            ft.Row([
+                ft.TextField(label="Song author", read_only=True, value=detail.song_author_name, expand=1),
+                ft.TextField(label="Map author", read_only=True, value=detail.level_author_name, expand=1),
+            ]),
             ft.Row([
                 *[ft.ElevatedButton(text=d.name, disabled=True) 
                 for d in detail.difficulties]
