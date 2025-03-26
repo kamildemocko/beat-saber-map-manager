23d4252a07e9e4c5e1f1ad32bcefd03de56ac5e5
Kamil Democko <kamil.democko@gmail.com>
2025-03-26 17:08:21 +0100

    get steamapps

--- src/beat_sabre_map_manager/data/bspath.py
+++ src/beat_sabre_map_manager/data/bspath.py
@@ -1,6 +1,10 @@
-from result import Result, Ok, Err
 from pathlib import Path
 import winreg
+import re
+
+
+class BSPathError(Exception):
+    ...
 
 
 class BSPath:
@@ -8,16 +12,16 @@ class BSPath:
         self.root: Path
         self.path: Path
     
-    def find_game_path(self) -> Result[Path, str]:
+    def find_game_path(self) -> Path:
         if self.path is None:
             ...
         
-        return Ok(self.path)
+        return self.path
         
         
 
     @staticmethod
-    def _get_steam_path() -> Result[Path, str]:
+    def _get_steam_path() -> Path:
         """tries to get the steam install path from the registry
 
         Returns:
@@ -32,11 +36,26 @@ class BSPath:
 
             install_path = winreg.QueryValueEx(key, r"InstallPath")
 
-            return Ok(Path(install_path[0]))
+            return Path(install_path[0])
 
-        except FileNotFoundError:
-            return Err("Steam install path not found")
+        except FileNotFoundError as fnfe:
+            raise BSPathError("Steam install path not found") from fnfe
 
     @staticmethod
-    def _get_steamapps_path() -> Result[Path, str]:
-        ...
+    def _get_steamapps_path(steam_root_path: Path) -> list[Path]:
+        """tries to get the steamapps path - or more from config file in steam folder
+
+        Args:
+            steam_root_path (Path): path to the steam root folder
+
+        Returns:
+            list[Path]: list of paths to steamapps folders
+        """
+        steam_conf_file = steam_root_path.join(r"steamapps\libraryfolders.vdf")
+        file_content = steam_conf_file.read_text()
+
+        pattern = re.compile(r'"path"\s+"(.*)"')
+        matches = pattern.findall(file_content)
+        matches_paths = [Path(m) for m in matches]
+
+        return [m for m in matches_paths if m.exists()]
